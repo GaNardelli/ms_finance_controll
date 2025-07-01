7d34e2f6bd27a77581ce99f074274427c9d5f056
Gabriel Nardelli <gv.nardelli@gmail.com>
2025-07-01 10:13:22 -0300

    configurando o sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
+import pymysql
 from flask import Flask, jsonify, request
-from server.controller.income_controller import incomeController;
+from server.controller.income_controller import incomeController
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+import configparser
 
+db = SQLAlchemy()
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read('config.ini')
+database_host = config['database']['host']
+database_password = config['database']['password']
+database_username = config['database']['username']
+userpass = 'mysql+pymysql://' + database_username + ':' + database_password + '@'
+server = 'localhost'
+dbname = '/fc'
+socket = '?unix_socket=/var/run/mysqld/mysqld.sock'
+app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname + socket
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+@app.route('/')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 @app.route("/income_list", methods=['GET'])
 def get_income_list():
